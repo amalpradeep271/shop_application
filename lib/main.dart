b59d58ab2468366595703b1ee2f494d4ee84f52e
Amal Pradeep <amalpradeep271@gmail.com>
2024-06-07 14:17:04 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
